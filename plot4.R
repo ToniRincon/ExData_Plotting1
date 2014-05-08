ab9d55f08de962a9121c86f05332621d4afee261
ToniRincon <antonio.rincon@gmail.com>
2014-05-08 21:48:05 +0200

    font size

--- plot4.R
+++ plot4.R
@@ -17,19 +17,19 @@ par(bg="transparent")
 par(mfrow=c(2,2))
 
 # plot on topleft
-plot(data$DateTime, data$Global_active_power, ylab="Golbal Active Power",type="l")
+plot(data$DateTime, data$Global_active_power, cex=0.6, xlab="", ylab="Golbal Active Power",type="l")
 
 # plot on topright
-plot(data$DateTime, data$Voltage, xlab="datetime", ylab="Voltage",type="l")
+plot(data$DateTime, data$Voltage, cex=0.6, xlab="datetime", ylab="Voltage",type="l")
 
 # plot on bottonleft
-plot(data$DateTime, data$Sub_metering_1, ylab="Enery sub metering",type="l",xlab="")
+plot(data$DateTime, data$Sub_metering_1, cex=0.6, xlab="", ylab="Enery sub metering", type="l")
 lines(data$DateTime, data$Sub_metering_2,col="red")
 lines(data$DateTime, data$Sub_metering_3,col="blue")
-legend("topright",bty = "n",cex=0.6,c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+legend("topright",bty = "n", cex=0.6, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
 
 # plot on bottomright
-plot(data$DateTime, data$Global_reactive_power, ylab="Global_reactive_power",type="l",xlab="datetime")
+plot(data$DateTime, data$Global_reactive_power, cex=0.6, ylab="Global_reactive_power",type="l",xlab="datetime")
 
 # save as plot4.png
 dev.copy(png,'plot4.png')
